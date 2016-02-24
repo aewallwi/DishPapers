564eb6c469e86c4c26398d88aac71787674eb1f2
nipanjana <Punthipatra@airbears2-10-142-40-168.airbears2.1918.berkeley.edu>
2016-02-23 17:11:14 -0800

    Adding the script for plotting subbands.

--- DishReflectometry/GB_reflectometry_part2/plot_reflectometry.py
+++ DishReflectometry/GB_reflectometry_part2/plot_reflectometry.py
@@ -21,11 +21,28 @@ for filename in sys.argv[1:]:
     fq,ph = fromcsv(ph_file)
     #d = 10**(db/10) * np.exp(2j*np.pi*ph/360) # power
     d = 10**(db/20) * np.exp(2j*np.pi*ph/360) # 20 to put into voltage amplitude, not power
+    valids = {
+          '100- 130 MHz'  : np.where(np.logical_and(fq>.100 ,fq<.130)), 
+          '130 - 160 MHz' : np.where(np.logical_and(fq>.130 ,fq<.160)), 
+          '160 - 190 MHz' : np.where(np.logical_and(fq>.160 ,fq<.190)),
+          }
+          
+
+for i,v in enumerate(valids.keys()):
 
     #valid = np.ones(fq.size, dtype=np.bool) # use entire sampled band
     #valid = np.where(fq < .250) # restrict to HERA band
-    valid = np.where(np.logical_and(fq < .170, fq > .130)) # restrict to PAPER band
-    fq, d, db, ph = fq[valid], d[valid], db[valid], ph[valid]
+    if v == '100-130 MHz': 
+    #valid1 = np.where(np.logical_and(fq < .130, fq > .100)) # restrict to PAPER band
+    #valid2 = np.where(np.logical_and(fq < .160, fq > .130)) # restrict to PAPER band
+    #valid3 = np.where(np.logical_and(fq < .190, fq > .160))
+#if i==0:
+      fq, d, db, ph = fq[valid[v]], d[valid[v]], db[valid[v]], ph[valid[v]]
+#elif i==1:
+ #   fq, d, db, ph = fq[valid2], d[valid2], db[valid2], ph[valid2]
+#elif i==2:    
+  #  fq, d, db, ph = fq[valid3], d[valid3], db[valid3], ph[valid3]
+    
     tau = np.fft.fftfreq(fq.size, fq[1]-fq[0])
     window = a.dsp.gen_window(fq.size, WINDOW)
     
@@ -67,13 +84,14 @@ for filename in sys.argv[1:]:
    #plt.plot(np.fft.fftshift(tau), 10.0*np.log10(np.fft.fftshift(np.abs(_d))), linewidth=2.5, label= 'Square Window')
     plt.plot(np.fft.fftshift(tau), 10.0*np.log10(np.fft.fftshift(np.abs(_dww))), linewidth=2.5, label=BASE.replace('_', ' '))
     
+    #plt.plot(fq, 10.0*np.log10((np.abs(_dww)**2)), label=BASE.replace('_', ' '))
     #plt.plot(fq, db,linewidth=2.5, label=BASE.replace('_', ' '))
     #plt.plot(fq, ph, linewidth=2.5, label=BASE.replace('_', ' '))
    
 
 #-----------------plotting returnloss magnitude--------------
 #plt.plot(fq, 10.0*np.log10((np.abs(d)**2)), label='Feed on dish')
-#plt.xlim(-350,350)
+plt.xlim(-350,350)
 plt.xlabel('Frequency (GHz)')
 plt.ylabel('Return loss magnitude (dB)')
 plt.grid()
